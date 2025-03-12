e62def34740a24858b64b6298e2fe712a4f85284
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-12 16:02:36 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,26 @@
+
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+
+variable "ami" {
+   type        = string
+   description = "Ubuntu AMI ID"
+   default     = "ami-0f5ee92e2d63afc18"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "My EC2 Instance"
+}
