2d92bd63840ce33f5c363d7d59098916c4459ed2
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-12 18:40:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "public_instance" {
 }
 resource "aws_instance" "harness" {
   # The ID of the EC2 instance that you want to import
-  instance_id = "i-0041c8a05c7fd58af"  # Replace with your actual instance ID
+  
 }
 
 terraform {
