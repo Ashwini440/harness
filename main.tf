fdaff8cc750cc25cd732af687e9af154c26b948b
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-12 18:37:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,11 @@ resource "aws_instance" "public_instance" {
    Name = var.name_tag,
  }
 }
+resource "aws_instance" "harness" {
+  # The ID of the EC2 instance that you want to import
+  instance_id = "i-0041c8a05c7fd58af"  # Replace with your actual instance ID
+}
+
 terraform {
   backend "s3" {
     bucket         = "harness-iacm-bucket"
