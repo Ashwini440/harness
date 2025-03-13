1cee3545542e3e696f9b00388c9b0f29a53150fd
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-13 10:18:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ resource "aws_instance" "harness" {
   # The ID of the EC2 instance that you want to import
    instance_type = "t2.micro"  
    ami = "ami-08b5b3a93ed654d19"
+tags = {
+    Name = var.name  }
 }
 
 terraform {
