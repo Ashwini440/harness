e38aac9d409d0484b4b94928399df17c894948cc
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-25 15:21:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,35 @@ user_data = <<-EOF
    Name = var.name_tag,
  }
 }
+resource "aws_security_group" "vm_sg" {
+  name        = "vm-security-group"
+  description = "Allow SSH and HTTP traffic"
 
+  # Allow SSH from anywhere (Restrict to your IP in production)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+security_groups = [aws_security_group.vm_sg.name]
 terraform {
   backend "s3" {
     bucket         = "test-iacm-bucket"
