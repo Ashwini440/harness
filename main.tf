74a6fbfc5259370d7b9be9c44b4a27513a73d4f4
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-25 14:44:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,18 @@ resource "aws_instance" "my_instance" {
  ami           = var.ami
  instance_type = var.instance_type
 
+user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt install -y unzip wget
+    wget -O delegate.tar.gz "https://app.harness.io/storage/harness-download/delegate/delegate.tar.gz"
+    mkdir /opt/harness-delegate
+    tar -xzf delegate.tar.gz -C /opt/harness-delegate
+    cd /opt/harness-delegate
+    chmod +x start.sh
+    nohup ./start.sh &
+  EOF
+
  tags = {
    Name = var.name_tag,
  }
